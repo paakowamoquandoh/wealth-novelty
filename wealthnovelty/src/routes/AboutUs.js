9e7d409acd184cec20484ba9aa8aeddaadaf86ac
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2024-06-13 12:07:38 +0000

    app init

---
+++ wealthnovelty/src/routes/AboutUs.js
@@ -0,0 +1,12 @@
+
+
+
+function AboutUs() {
+    return(
+        <>
+       <h1>About Us</h1>
+        </>
+    )    
+}
+
+export default AboutUs;
---
+++ wealthnovelty/src/routes/ContactUs.js
@@ -0,0 +1,12 @@
+
+
+
+function contactUs() {
+    return(
+        <>
+       <h1>contactUs</h1>
+        </>
+    )    
+}
+
+export default contactUs;
