6d84e9352d037e7687b983f54d1982ab16fcb247
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2025-04-04 20:49:24 +0000

    Update rentalItem.jsx

--- client/src/components/UI/rentalItem.jsx
+++ client/src/components/UI/rentalItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import "../../styles/car-item.css";
 
 const RentalItem = ({ item }) => {
-  const { photos, model, name, transmission, engine, price, distance } = item;
+  const { photos, model, name, transmission, engine, cheapestPrice, distance } = item;
   const imageUrl = photos.length > 0 ? photos[0] : 'default-image-url'; // Fallback to a default image if no photos are available
 
   return (
@@ -16,7 +16,7 @@ const RentalItem = ({ item }) => {
         <div className="car__item-content mt-4">
           <h4 className="section__title text-center">{`${model} ${name}`}</h4>
           <h6 className="rent__price text-center mt-">
-            Ghc{price}<span>/ Day</span>
+            Ghc{cheapestPrice}<span>/ Day</span>
           </h6>
           <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
             <span className="d-flex align-items-center gap-1">
