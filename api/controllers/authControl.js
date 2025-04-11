2ec735a44fa289e2957120702f735ea0822a7447
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2025-04-11 22:12:21 +0000

    Update authControl.js

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -30,6 +30,8 @@ export const login = async (req, res, next) => {
         const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
         const { password, isAdmin, ...otherDetails } = user._doc
+        console.log("hurray");
+        
         res.cookie("access_token", token, {
             httpOnly: true,
             secure: true,
